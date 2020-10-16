cdf72e78db4b8a9430d90c6f790c91c809264293
websites <sungwontoto@kookmin.ac.kr>
2020-10-16 23:35:47 +0900

    Add features that show firebase data information on bucket Activity

--- app/src/main/java/com/example/shoppingmall/BucketActivity.kt
+++ app/src/main/java/com/example/shoppingmall/BucketActivity.kt
@@ -1,22 +1,121 @@
 package com.example.shoppingmall
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_bucket.*
+
+
+// 데이터 베이스 접근을 위한 변수 설정
+
+
+
 
 class BucketActivity : AppCompatActivity() {
+    val database = Firebase.database
+    var myRef = database.getReference("itemList")
+
+    val mRootDatabaseReference = FirebaseDatabase.getInstance().reference
+    val mItemListDataSet =
+        mRootDatabaseReference.child("itemList") //닉네임 담아놀 하위 데이터베이스
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bucket)
         setTitle("나의 장바구니")
 
-        val btnHome: Button = findViewById(R.id.gohome)
-        val btnPurchase: Button = findViewById(R.id.gopurchase)
+        //구매 페이지, 메인 페이지로 이동하기 위한 버튼 변수
+        val btnHome: Button = findViewById(R.id.goHome)
+        val btnPurchase: Button = findViewById(R.id.goPurchase)
+        val btnDelete: Button = findViewById(R.id.deleteItem)
 
+        var myItem = ArrayList<ItemList>()
+
+
+        mItemListDataSet.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(dataSnapshot: DatabaseError) {
+            }
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                myItem.clear()
+                for(snapshot in dataSnapshot.children) {
+                    // 키 값을 비교해서 일치하면 ArrayList에 값을 차례대로 할당하여 추가한다.
+                    when {
+                        snapshot.key.toString() == "iPad" -> myItem.add(ItemList(snapshot.key.toString(),snapshot.value.toString(),"ipad"))
+                        snapshot.key.toString() == "macBook" -> myItem.add(ItemList(snapshot.key.toString(),snapshot.value.toString(),"macbook"))
+                        snapshot.key.toString() == "airPods" -> myItem.add(ItemList(snapshot.key.toString(),snapshot.value.toString(),"airpods"))
+                        snapshot.key.toString() == "appleWatch" -> myItem.add(ItemList(snapshot.key.toString(),snapshot.value.toString(),"watch"))
+                    }
+
+                }
+                //리스트뷰에 어댑터를 연결 후 값을 렌더링 한다.
+                val itemAdapter = MainListAdapter(this@BucketActivity,myItem)
+                view_item.adapter = itemAdapter
+//                btnDelete.setOnClickListener {
+//                    for(snapshot in myItem) {
+//
+//                    }
+//                }
+            }
+
+        })
+
+
+
+        //메인 페이지로 이동
         btnHome.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
+
+}
+
+
+
+//리스트뷰 안의 내용을 구성할 변수를 담는 클래스
+class ItemList(val itemName: String, val itemPrice: String, val photo: String)
+
+// Adapter를 만들기 위한 클래스
+class MainListAdapter (val context: Context, val myItem: ArrayList<ItemList>) : BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        //item을 Adapter에서 사용할 View형태로 부풀려주는 역할
+        val view: View = LayoutInflater.from(context).inflate(R.layout.main_layout_item, null)
+
+        // 위에서 생성된 View를 layout_item.xml 파일의 각 뷰와 연결
+        val itemTitle = view.findViewById<TextView>(R.id.item_title)
+        val itemPrice = view.findViewById<TextView>(R.id.item_price)
+        val itemImg = view.findViewById<ImageView>(R.id.item_img)
+
+        // 이미지, 상품 데이터를 ImageView, TextView 안에 담는다.
+        val items = myItem[position]
+        val resourceID = context.resources.getIdentifier(items.photo, "drawable", context.packageName)
+        itemImg.setImageResource(resourceID)
+        itemTitle.text = items.itemName
+        itemPrice.text = items.itemPrice
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return myItem[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return myItem.size
+    }
+
 }
--- app/src/main/java/com/example/shoppingmall/MainActivity.kt
+++ app/src/main/java/com/example/shoppingmall/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.example.shoppingmall
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.d
+import android.view.ContextThemeWrapper
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    // database 연동하기 위한 변수 설정
+    // itemList 이름을 가진 경로를 myRef 변수에 설정
+    val database = Firebase.database
+    val myRef = database.getReference("itemList")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setTitle("쇼핑몰 홈")
-
-
 
         val btnPurchase: Button = findViewById(R.id.purchase) // 구매 버튼 변수
         val btnBucket: Button = findViewById(R.id.bucket) // 장바구니 버튼 변수
@@ -28,9 +41,10 @@ class MainActivity : AppCompatActivity() {
         val macBook: CheckBox = findViewById(R.id.img3_check)
         val airPods: CheckBox = findViewById(R.id.img4_check)
 
-        val listItems = mapOf<String, Int>("iPad" to 1190000, "appleWatch" to 359000, "macBook" to 1750000, "airPods" to 150000)
 
         var price: Int = 0 ; // 처음에 제품 가격의 합을 0으로 초기화
+
+        // 구매 버튼 클릭 시 기능 구현
         btnPurchase.setOnClickListener {
             if(iPad.isChecked == true) {
                 price += 1190000
@@ -45,19 +59,46 @@ class MainActivity : AppCompatActivity() {
                 price += 150000;
             }
 
-//            val intent = Intent(this, MainActivity::class.java);
-//            intent.putExtra("prices", Integer.parseInt(price.toString()))
             Toast.makeText(this, "총합: "+price, Toast.LENGTH_SHORT).show()
-            price = 0;
 
             val intent = Intent(this, PurchaseActivity::class.java)
             startActivityForResult(intent, 0)
 
+            price = 0
         }
-        btnBucket.setOnClickListener {
-            val intent = Intent(this, BucketActivity::class.java)
 
-            startActivityForResult(intent, 0)
+        // 장바구니 버튼 클릭 시 기능 구현
+        btnBucket.setOnClickListener {
+
+            //장바구니 버튼 클릭 시 장바구니 페이지로 넘어갈 것인지 확인하는 다이얼로그 창 보여주기
+            val builder = AlertDialog.Builder(ContextThemeWrapper(this, R.style.Theme_AppCompat_Light_Dialog))
+            builder.setTitle("장바구니 페이지로 이동하시겠습니까?")
+            builder.setMessage("이동 시 선택된 상품이 장바구니에 저장됩니다.")
+
+            builder.setPositiveButton("확인") { _, _ ->
+                //데이터 베이스에 체크되어 있는 상품의 이름과 가격을 key : value 형태로 저장
+                if(iPad.isChecked == true) {
+                    myRef.child("iPad").setValue("1190000")
+                }
+                if(appleWatch.isChecked == true) {
+                    myRef.child("appleWatch").setValue("359000")
+                }
+                if(macBook.isChecked == true) {
+                    myRef.child("macBook").setValue("1750000")
+                }
+                if(airPods.isChecked == true) {
+                    myRef.child("airPods").setValue("150000")
+                }
+
+                // 장바구니 페이지로 이동하기
+                val intent = Intent(this, BucketActivity::class.java)
+                startActivityForResult(intent, 0)
+            }
+            builder.setNegativeButton("취소") { _, _ ->
+                //취소 버튼 눌렀을 때에는 액션을 하지 않음
+            }
+            builder.show()
+
         }
 
     }
